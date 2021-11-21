3fb86c334a336a672dd2f1c609ed743643344142
Anthony N <antnat96@gmail.com>
2021-11-21 18:15:54 -0500

    send light and position data from client to server

--- analytics_engine.py
+++ analytics_engine.py
@@ -7,23 +7,30 @@ from datetime import datetime
 
 id = 'csci_ecu_21_ant'
 name = id + 'analytics_engine'
-topic = id + '/data'
+topic = id + '/fpu_data'
 mqtt_client = mqtt.Client(name)
 mqtt_client.connect('35.243.224.151')
 mqtt_client.subscribe(topic)
 mqtt_client.loop_start()
-
 file_name = 'fpu_data.csv'
 fieldnames = ['date', 'time', 'wattage', 'x', 'y', 'z']
+curr_uv = 0
 
-def handle_telemetry(client, userdata, message):
+def handle_fpu_data(client, userdata, message):
+    global curr_uv
     payload = json.loads(message.payload.decode())
-    print("Message received:", payload)
+    print("handle_fpu_data:", payload)
+    if payload['uv'] > curr_uv:
+        curr_uv = payload['uv']
+        print('increase x, decrease z')
+    if payload['uv'] < curr_uv:
+        curr_uv = payload['uv']
+        print('increase z, decrease y')
     # with open(file_name, mode='a') as temperature_file:        
     #     temperature_writer = csv.DictWriter(temperature_file, fieldnames=fieldnames)
     #     temperature_writer.writerow({'date' : datetime.now().astimezone().replace(microsecond=0).isoformat(), 'temperature' : payload['temperature']})
 
-mqtt_client.on_message = handle_telemetry
+mqtt_client.on_message = handle_fpu_data
 
 # if not path.exists(file_name):
 #     with open(file_name, mode='w') as csv_file:
--- field_processing_unit.py
+++ field_processing_unit.py
@@ -5,17 +5,41 @@ import paho.mqtt.client as mqtt
 from counterfit_shims_grove import GroveLightSensor
 CounterFitConnection.init('127.0.0.1', 5000)
 
+sensor = GroveLightSensor(1)
+position_x = 45
+position_z = 45
+
+def handle_fpu_commands(client, userdata, message, position_x, position_z):
+    payload = json.loads(message.payload.decode())
+    print("handle_fpu_commands:", payload)
+    if payload['desired_position_x'] != position_x:
+        print("Moving on x-axis...")
+        position_x = payload['desired_position_x']
+    if payload['desired_position_z'] != position_z:
+        print("Moving on z-axis...")
+        position_z = payload['desired_position_z']
+    print("New position_x: ", position_x)
+    print("New position_z: ", position_z)
+
 id = 'csci_ecu_21_ant'
 name = id + 'field_processing_unit'
-topic = id + '/data'
+outbound_topic = id + '/fpu_data'
+inbound_topic = id + '/fpu_commands'
 mqtt_client = mqtt.Client(name)
 mqtt_client.connect('35.243.224.151')
+mqtt_client.subscribe(inbound_topic)
 mqtt_client.loop_start()
-sensor = GroveLightSensor(1)
+mqtt_client.on_message = handle_fpu_commands
 
 while True:
-    light = sensor.light
-    data = json.dumps({'light' : light})
+    uv = sensor.light
+    data = json.dumps(
+        {
+        'uv' : uv, 
+        'position_x': position_x, 
+        'position_z': position_z
+        }
+    )
     print(data)
-    mqtt_client.publish(topic, data)
+    mqtt_client.publish(outbound_topic, data)
     time.sleep(5)
